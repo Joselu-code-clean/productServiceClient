1a20ede9aa76e67a83bef96895187ee58628cf73
Joselu-code-clean <71699651+Joselu-code-clean@users.noreply.github.com>
2021-11-15 10:50:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# microservicios-Cliente
+ Cliente para la practica 1 de microservicios
---
+++ productServiceClient/src/test/java/com/example/productServiceClient/ProductServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productServiceClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
